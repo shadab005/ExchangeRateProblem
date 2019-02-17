3a7d94c2905dc3a527466e075f5ba865d19bb975
Sudhanshu Pratap Singh <sudhanshu.singh@SW-LP01736.local>
2019-02-17 23:29:09 +0530

    Adding kafka

--- src/main/java/com/example/demo/handler/ExchangeRateMessageHandler.java
+++ src/main/java/com/example/demo/handler/ExchangeRateMessageHandler.java
@@ -3,14 +3,23 @@ package com.example.demo.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @Slf4j
 public class ExchangeRateMessageHandler {
 
-    @RabbitListener(queues = "${erp.rabbitmq.queue}")
+    //@RabbitListener(queues = "${erp.rabbitmq.queue}")
     public void processOrder(String message) {
-        log.info("Message received for batch: "+ message);
+        log.info("RMQ Message received for batch: "+ message);
     }
+
+    @KafkaListener(topics = "exchange-rate", groupId = "exchange_rate_group")
+    public void consume(String message) throws IOException {
+        log.info("Kafka Message received for batch: "+ message);
+    }
+
 }
--- src/main/java/com/example/demo/job/step/ExchangeRateJobListener.java
+++ src/main/java/com/example/demo/job/step/ExchangeRateJobListener.java
@@ -7,7 +7,10 @@ import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @Component
 @Slf4j
@@ -22,6 +25,11 @@ public class ExchangeRateJobListener implements JobExecutionListener {
     @Value("${erp.rabbitmq.routingkey}")
     private String routingkey;
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${kafka.topic.exchange-rate}")
+    String topic;
 
     @Override
     public void beforeJob(JobExecution jobExecution) {
@@ -32,7 +40,21 @@ public class ExchangeRateJobListener implements JobExecutionListener {
     public void afterJob(JobExecution jobExecution) {
         StepExecution stepExecution = jobExecution.getStepExecutions().iterator().next();
         String batchId = stepExecution.getExecutionContext().getString("batchId");
+
         log.info("Exchange Rate Job finished successfully with batch Id : " + batchId);
-        rabbitTemplate.convertAndSend(exchange, routingkey, "batchId:"+batchId);
+        //rabbitTemplate.convertAndSend(exchange, routingkey, "batchId:"+batchId);
+
+
+        log.info("Exchange Rate Job finished successfully with batch Id : " + batchId);
+        ListenableFuture<SendResult<String, String>> future = this.kafkaTemplate.send(topic, batchId);
+        future.addCallback(result -> {
+                    log.info("Successfully published to topic: " + topic +
+                            " partition: " + result.getRecordMetadata().partition() +
+                            " offset: " + result.getRecordMetadata().offset());
+                }
+                , ex -> {
+                    log.error("Failed to publish to " + topic, ex);
+                });
+
     }
 }
